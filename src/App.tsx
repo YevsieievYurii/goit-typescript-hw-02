5295683d258765a1fcb5cb202346d48a967a8f5b
Yurii <evseevyrij90@gmail.com>
2025-05-18 19:59:21 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
 import "./App.css";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Image Search App</h1>
+      {/* Тут будуть твої компоненти з попередньої домашки */}
+      {/* <Searchbar /> */}
+      {/* <ImageGallery /> */}
+    </div>
+  );
+};
+
+export default App;
